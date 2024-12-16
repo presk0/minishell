8892127e0237e7624202f0a837c07e8c7c4e541e
Nicolas Dionis <nidionis@k1r4p3.42mulhouse.fr>
2024-12-16 18:05:11 +0100

    pipe_split is used by btree_apply_prefix generating a tree around pipes

--- src/main.c
+++ src/main.c
@@ -43,48 +43,16 @@ size_t	substitute_var(char *str, t_list *gc)
 	return (ft_strlen(tmp));
 }
 
-// char	*substitute_vars(char *str, t_list *gc)
-// {
-// 	char	*new_str;
-// 	char	*tmp;
-// 	size_t	len_max;
-// 	size_t	i;
-
-// 	len_max = ft_strlen(str) * 2 + 1024;
-// 	if (!str)
-// 		return (NULL);
-// 	new_str = NULL;
-// 	new_str = gc_calloc(&gc, len_max, sizeof(char));
-// 	if (!new_str)
-// 		minishell_exit(gc);
-// 	i = 0;
-// 	while (*str)
-// 	{
-// 		if (i >= len_max)
-// 		{
-// 			tmp = gc_calloc(&gc, len_max, sizeof(char));
-// 			if (!tmp)
-// 				minishell_exit(gc);
-// 			ft_strlcpy(tmp, new_str, len_max);
-// 			gc_free_item(&gc, new_str);	
-// 			new_str = tmp;
-// 		}
-// 		if (*str == '$')
-// 		{
-// 			ft_strlcat(new_str, str, len_max);
-// 			i += substitute_var(new_str + i, gc);
-// 			str = end_of_word(++str);
-// 		}
-// 		else
-// 			new_str[i++] = *str++;
-// 	}
-// 	return (new_str);
-// }
 char	*substr_left(char *node_content)
 {
 	char	*ret;
 
 	ret = ft_substr(node_content, 0, ft_strchr(node_content, '|') - node_content);
+	if (ret && !*ret)
+	{
+		free(ret);
+		ret = NULL;
+	}
 	return (ret);
 }
 char	*substr_right(char *node_content)
@@ -92,6 +60,11 @@ char	*substr_right(char *node_content)
 	char	*ret;
 
 	ret = ft_substr(node_content, ft_strchr(node_content, '|') - node_content + 1, ft_strlen(node_content));
+	if (ret && !*ret)
+	{
+		free(ret);
+		ret = NULL;
+	}
 	return (ret);
 }
 void	*pipe_split(t_btree *node)
@@ -104,17 +77,28 @@ void	*pipe_split(t_btree *node)
 
 	if (!node)
 		return (NULL);
+	left = NULL;
+	right = NULL;
 	node_content = (char *)node->content;
+ 	if (!ft_strchr(node_content, '|'))
+		return (NULL);
 	left_content = substr_left(node_content);
 	right_content = substr_right(node_content);
-	left = btree_create_node(left_content);
-	right = btree_create_node(right_content);
-	if (left)
+	if (left_content)
+		left = btree_create_node(left_content);
+	if (right_content)
+		right = btree_create_node(right_content);
+	if (node->left)
 		ft_errmsg("[pipe_split] create left node but one already exists\n");
 	node->left = left;
-	if (right)
+	if (node->right)
 		ft_errmsg("[pipe_split] create right node but one already exists\n");
 	node->right = right;
+	if (node_content)
+	{
+		*node_content = '|';
+		node_content[1] = '\0';
+	}
 	return (NULL);
 }
 
@@ -126,13 +110,12 @@ void	*pipe_split(t_btree *node)
 void	apply_cmd(char *line, t_list *gc)
 {
 	t_btree	*cmd_tree;
+	(void)gc;
 
 	cmd_tree = btree_create_node(line);
-	//btree_split_node(cmd_tree, substr_split);
 	btree_apply_prefix(cmd_tree, pipe_split);
-	gc_append(&gc, cmd_tree);
 	display_tree(cmd_tree);
-	free_tree(cmd_tree, &free);
+	free_tree(cmd_tree, free);
 }
 
 void	minishell(void)
@@ -162,10 +145,11 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	(void)envp;
+	char	*line = ft_strdup(argv[1]);
 	//minishell();
-	//apply_cmd(argv[1], NULL);
-	printf("[substr_left] %s\n", substr_left(argv[1]));
-	printf("[substr_right] %s\n", substr_right(argv[1]));
+	apply_cmd(line, NULL);
+	//printf("[substr_left] %s\n", substr_left(argv[1]));
+	//printf("[substr_right] %s\n", substr_right(argv[1]));
 	return (0);
 }
 
