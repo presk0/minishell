282f0fce4287ad02d6c7ff0cd3cb600d0dc856d0
presko <presko@localhost.localdomain>
2025-01-10 16:54:44 +0100

    [buggy] not that bad but pb when '/bin/ls | /bin/cat -e | /bin/cat -e' twice: $/bin/ls | /bin/cat -e | /bin/cat -e includes115581 lib115581 Makefile115581 minishell115581 src115581 tags115581 TO_DO.txt115581 $/bin/ls includes  lib  Makefile  minishell  src  tags  TO_DO.txt $/bin/ls includes  lib  Makefile  minishell  src  tags  TO_DO.txt $/bin/ls | /bin/cat -e | /bin/cat -e free(): invalid pointer Aborted

--- src/ft_exec_forking.c
+++ src/ft_exec_forking.c
@@ -16,15 +16,19 @@ void	run_line(t_list *gc, char *line, t_env *env)
 {
 	t_btree			*cmd_tree;
 	t_btree_content	*content;
+	char			*line_cpy;
 
 	(void)cmd_tree;
 	content = gc_malloc(&gc, 1, sizeof(t_btree_content));
 	if (!content)
 		minishell_exit(gc);
-	content->cmd = ft_strdup(line);
-	gc_append(&gc, content->cmd);
+	line_cpy = ft_strdup(line);
+	content->cmd = line_cpy;
+	gc_append(&gc, line_cpy);
 	cmd_tree = new_node(gc, content);
 	exec_forking(gc, cmd_tree, env);
+	gc_free_item(&gc, line_cpy);
+	gc_free_tree(&gc, &cmd_tree, gc_free_node_content);
 }
 
 void	exec_forking(t_list *gc, t_btree *cmd_tree, t_env *env)
@@ -33,23 +37,25 @@ void	exec_forking(t_list *gc, t_btree *cmd_tree, t_env *env)
 	int status;
 	char			*sep;
 
+	(void)pid;
+	(void)status;
 	sep = ft_strdup("|");
 	gc_append(&gc, sep);
 	btree_split(gc, cmd_tree, sep);
 	if (check_childs(gc, cmd_tree)) 
 	{
 		rec_tokenization(gc, cmd_tree, env);
-		pid = fork();
-		if (pid == -1) {
-			perror("[exec_forking] fork failed");
-			minishell_exit(gc);
-		}
-		if (pid == 0) {
-			init_sig(gc);
+//		pid = fork();
+//		if (pid == -1) {
+//			perror("[exec_forking] fork failed");
+//			minishell_exit(gc);
+//		}
+//		if (pid == 0) {
+//			init_sig(gc);
 			rec_exec(gc, cmd_tree, env);
-		}
-		waitpid(pid, &status, 0);
+//			exit(-42);
+//		}
+//		waitpid(pid, &status, 0);
 	}
-	gc_free_tree(&gc, &cmd_tree, gc_free_node_content);
 	gc_free_item(&gc, sep);
 }
--- src/ft_exec_recursive.c
+++ src/ft_exec_recursive.c
@@ -14,13 +14,14 @@
 
 void	exec_cmd(t_list *gc, t_token *tok, t_env *env)
 {
+	(void)gc;
 	if (!tok)
 		return ;
 	handle_redir_in(tok);
 	handle_redir_out(tok);
 	execve(tok->cmd, tok->args, env->my_env);
 	perror("[exec_cmd] execve failed");
-	minishell_exit(gc);
+	//minishell_exit(gc);
 }
 
 void	exec_content(t_list *gc, t_btree *node, t_env *env)
@@ -30,50 +31,110 @@ void	exec_content(t_list *gc, t_btree *node, t_env *env)
 	exec_cmd(gc, tok, env);
 }
 
+//void rec_exec(t_list *gc, t_btree *node, t_env *envp)
+//{
+//	int pipe_fd[2];
+//	pid_t pid;
+//	int status;
+//
+//	if (!check_childs(gc, node) || !node)
+//		return ;
+//	if (is_pipe(node))
+//	{
+//		if (pipe(pipe_fd) == -1) {
+//			perror("[rec_exec] pipe failed");
+//			minishell_exit(gc);
+//		}
+//		pid = fork();
+//		if (pid == -1)
+//		{
+//			perror("[rec_exec] fork failed");
+//			minishell_exit(gc);
+//		}
+//		if (pid == 0)
+//		{
+//			close(pipe_fd[0]);
+//			dup2(pipe_fd[1], STDOUT_FILENO);
+//			close(pipe_fd[1]);
+//			rec_exec(gc, node->left, envp);
+//			minishell_exit(gc);
+//		}
+//		close(pipe_fd[1]);
+//		dup2(pipe_fd[0], STDIN_FILENO);
+//		close(pipe_fd[0]);
+//		waitpid(pid, &status, 0);
+//		rec_exec(gc, node->right, envp);
+//	} else {
+//		pid = fork();
+//		if (pid == -1) {
+//			perror("[rec_exec] fork failed");
+//			minishell_exit(gc);
+//		}
+//		if (pid == 0) {
+//			exec_content(gc, node, envp);
+//			exit(-42);
+//			//minishell_exit(gc);
+//		}
+//		waitpid(pid, &status, 0);
+//	}
+//}
+
 void rec_exec(t_list *gc, t_btree *node, t_env *envp)
 {
-	int pipe_fd[2];
-	pid_t pid;
-	int status;
+    int pipe_fd[2];
+    pid_t pid;
+    int status;
+    int stdin_fd; // To save the original stdin
 
-	if (!check_childs(gc, node) || !node)
-		return ;
-	if (is_pipe(node))
-	{
-		if (pipe(pipe_fd) == -1) {
-			perror("[rec_exec] pipe failed");
+    stdin_fd = dup(STDIN_FILENO);
+    if (stdin_fd == -1) {
+        perror("[rec_exec] dup failed");
+        minishell_exit(gc);
+    }
+    if (is_pipe(node))
+    {
+        if (pipe(pipe_fd) == -1) {
+            perror("[rec_exec] pipe failed");
+            minishell_exit(gc);
+        }
+        pid = fork();
+        if (pid == -1) {
+            perror("[rec_exec] fork failed");
+            minishell_exit(gc);
+        }
+        if (pid == 0) {
+            close(pipe_fd[0]);
+            dup2(pipe_fd[1], STDOUT_FILENO);
+            close(pipe_fd[1]);
+            rec_exec(gc, node->left, envp);
+            minishell_exit(gc);
+        }
+
+        close(pipe_fd[1]);
+        dup2(pipe_fd[0], STDIN_FILENO);
+        close(pipe_fd[0]);
+        rec_exec(gc, node->right, envp);
+        waitpid(pid, &status, 0);
+    }
+    else
+    {
+        pid = fork();
+        if (pid == -1) {
+            perror("[rec_exec] fork failed");
+            minishell_exit(gc);
+        }
+
+        if (pid == 0) {
+            exec_content(gc, node, envp);
 			minishell_exit(gc);
-		}
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("[rec_exec] fork failed");
-			minishell_exit(gc);
-		}
-		if (pid == 0)
-		{
-			close(pipe_fd[0]);
-			dup2(pipe_fd[1], STDOUT_FILENO);
-			close(pipe_fd[1]);
-			rec_exec(gc, node->left, envp);
-			minishell_exit(gc);
-		}
-		close(pipe_fd[1]);
-		dup2(pipe_fd[0], STDIN_FILENO);
-		close(pipe_fd[0]);
-		waitpid(pid, &status, 0);
-		rec_exec(gc, node->right, envp);
-	} else {
-		pid = fork();
-		if (pid == -1) {
-			perror("[rec_exec] fork failed");
-			minishell_exit(gc);
-		}
-		if (pid == 0) {
-			exec_content(gc, node, envp);
-			minishell_exit(gc);
-		}
-		waitpid(pid, &status, 0);
-	}
+        }
+
+        waitpid(pid, &status, 0);
+    }
+    if (dup2(stdin_fd, STDIN_FILENO) == -1) {
+        perror("[rec_exec] dup2 failed");
+        minishell_exit(gc);
+    }
+    close(stdin_fd);
 }
 
