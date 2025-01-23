d746ddb5a1c301e3c074c9d40b79b163a60547d8
Nicolas Dionis <nidionis@k1r4p10.42mulhouse.fr>
2025-01-23 21:19:47 +0100

    fairly no leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nkieffer <nkieffer@student.42.fr>			+#+  +:+		+#+		*/
 /*												+#+#+#+#+#+   +#+			*/
 /*   Created: 2024/09/04 16:20:59 by nidionis			#+#	#+#				*/
-/*   Updated: 2025/01/23 19:46:53 by nidionis         ###   ########.fr       */
+/*   Updated: 2025/01/23 21:18:34 by nidionis         ###   ########.fr       */
 /*																			*/
 /* ************************************************************************** */
 
@@ -26,18 +26,21 @@ char	**duplicate_tab(char **tab_original)
 {
 	char	**tab_copy;
 	size_t	tab_len;
+	size_t	i;
 
 	tab_len = ft_tablen(tab_original);
 	tab_copy = gc_malloc(&d.gc, (tab_len + 1), sizeof(char *));
 	tab_copy[tab_len] = NULL;
-	while (tab_len--)
+	i = 0;
+	while (i < tab_len)
 	{
-		tab_copy[tab_len] = gc_strdup(&d.gc, tab_original[tab_len]);
-		if (!tab_copy[tab_len])
+		tab_copy[i] = gc_strdup(&d.gc, tab_original[i]);
+		if (!tab_copy[i])
 		{
 			ft_errmsg("[duplicate_tab]malloc error\n");
-			minishell_exit("", ERR_GC_STRDUP);
+			minishell_exit("[duplicate_tab]", ERR_GC_STRDUP);
 		}
+		i++;
 	}
 	return (tab_copy);
 }
@@ -47,8 +50,8 @@ void	minishell(char **envp)
 	char	*line;
 
 	init_sig();
-	d.env = duplicate_tab(envp);
 	d.gc = NULL;
+	d.env = duplicate_tab(envp);
 	while (1)
 	{
 		line = readline(PS1);
